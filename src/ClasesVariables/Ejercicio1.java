639557482d880e37d12bfb8b1495e2567ae3d44a
Jhoan Sebastian Herrera Molina <sebastian.herrera.molina@hotmail.com>
2021-04-07 08:27:44 -0500

    Add files via upload

---
+++ src/ClasesVariables/Ejercicio1.java
@@ -0,0 +1,23 @@
+package ClasesVariables;
+
+public class Ejercicio1 {
+	
+	static final double dolar = 3636.50; //variable constante
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String nombre = "Juan Camilo";
+		double x = Math.round(( Math.random())*100.0)/100.0;
+		float pi = (float)Math.PI;
+		double euler = Math.E;
+        
+		System.out.println("Este es el Nombre guardado "+nombre);
+		System.out.println("Esta variable es aleatoria y solo muestra dos decimales "+x);
+		System.out.println("Esta variable es constante y muesta el valor del dolar "+dolar);//frente al peso Colombiano en 04/07/2021
+		System.out.println("Esta es el valor de PI con 7 decimales "+pi);
+		System.out.println("Este es el valor de Euler con 15 decimales "+euler);
+
+
+	}
+
+}
